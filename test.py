7e2325efed6464c78d484ba9022684a56af1a280
Jason-8577 <jasono119@nycstudents.net>
2025-10-09 06:23:40 -0700

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,18 @@
     return both 
 print(occupied(5, "CC..C", ".CC..")) """
 
-def maximum(H,O,N,I,n):
-    for i in range
+""" Ex.(example): """
+""" student = {
+    'name': 'Cadee',
+    'age' : 15, 
+    'grades' : (80,90,100)
+}
+print(student['grades']) """
+
+item = {
+    "name": "Macbook",
+    "price": 1299.99,
+    "department": "Laptop",
+    "description": "13-inch, 528GB."
+}
+print(item["name"])

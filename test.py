4a114144aa2106fbaaa1078da8cfe18368350e39
Jason-8577 <jasono119@nycstudents.net>
2025-10-09 06:01:07 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-item = {
-    "name": "Tv" or "Television"/"iphone",
-        "price": 
-}
+""" def occupied(n,y,t):
+    both = 0
+    for i in range(n):
+        if (y[i] == "C" and t[i] == "C"):
+            both +=1
+    return both 
+print(occupied(5, "CC..C", ".CC..")) """
+
+def maximum(H,O,N,I,n):
+    for i in range

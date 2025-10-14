a908f48baded6c338d6d55c16443224c54dd8104
Jason-8577 <jasono119@nycstudents.net>
2025-10-14 06:24:22 -0700

    Update test.py

--- test.py
+++ test.py
@@ -14,10 +14,37 @@ print(occupied(5, "CC..C", ".CC..")) """
 }
 print(student['grades']) """
 
+""" def password(x):
+    upper = 0
+    lower = 0
+    digits = 0
+    if len(x) > 6 and len(x) < 13:
+        for char in x:
+            if char.isdigit():
+                lower += 1
+            if upper> 3 and lower >2 and digits> 1 """
+
+""" def wizard(o, n, duels):
+    owner = 0
+    number_of_owner = 1
+    for i in duels:
+        #"BA"
+        if duels[1] == owner:
+            owner = duels[0]
+            number_of_owner += 1
+    for i in range(n):
+        if duels[i][1] == owner """
+
 item = {
     "name": "Macbook",
     "price": 1299.99,
     "department": "Laptop",
     "description": "13-inch, 528GB."
 }
-print(item["name"])
+{
+    "name": "Hp laptop",
+    "price": 799.99,
+    "department": "Laptop",
+    "description": "15-inch, 256GB."
+}
+print(item["name"][0])

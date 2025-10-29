fed544decf285b7584b449cda69297f5035875d5
Jason-8577 <jasono119@nycstudents.net>
2025-10-29 06:22:00 -0700

    Update test.py

--- test.py
+++ test.py
@@ -27,33 +27,53 @@ while best_buy_items:
     Purchase = input("What would you like to buy?")
     Purchase_history.append(Purchase)
     if Purchase == "Macbook":
-        print("added to cart", Purchase_history, total + 1299.99)
+        print("added to cart", Purchase_history)
+        total += 1299.99
     elif Purchase == "Hp laptop":
-        print("added to cart", Purchase_history, total + 799.99)
+        print("added to cart", Purchase_history)
+        total += 799.99
     elif Purchase == "Lenovo Chromebook 14 laptop":
-        print("added to cart", Purchase_history, total + 249.99)
+        print("added to cart", Purchase_history)
+        total += 249.99
     elif Purchase or Continue == "done":
         print(Purchase_history, total)
         break
     else:
         print("Item not found")
     Continue = input("Do you wish to continue?")
-    if Continue == "no":
+    if Continue == "no" or "No":
         print(Purchase_history, total)
         break
-    elif Continue == "yes":
+    elif Continue == "yes" or "Yes":
         Purchase = input("What else would you like to buy?")
         Purchase_history.append(Purchase)
         if Purchase == "Macbook":
-            print("added to cart", Purchase_history, total + 1299.99)
+            print("added to cart", Purchase_history)
+            total += 1299.99
         elif Purchase == "Hp laptop":
-            print("added to cart", Purchase_history, total + 799.99)
+            print("added to cart", Purchase_history)
+            total += 799.99
         elif Purchase == "Lenovo Chromebook 14 laptop":
-            print("added to cart", Purchase_history, total + 249.99)
+            print("added to cart", Purchase_history)
+            total += 249.99
     elif Purchase or Continue == "done":
         print(Purchase_history, total)
         break
     Continue = input("Are you done shopping?")
-    if Continue == "yes":
+    if Continue == "yes" or "Yes":
         print(Purchase_history, total)
         break
+    elif Continue == "no" or "No":
+        Purchase = input("What else would you like to buy?")
+        Purchase_history.append(Purchase)
+        if Purchase == "Macbook":
+            print("added to cart", Purchase_history)
+            total += 1299.99
+        elif Purchase == "Hp laptop":
+            print("added to cart", Purchase_history)
+            total += 799.99
+        elif Purchase == "Lenovo Chromebook 14 laptop":
+            print("added to cart", Purchase_history)
+            total += 249.99
+            print(Purchase_history, total), print("Thank you for shopping here")
+            break
